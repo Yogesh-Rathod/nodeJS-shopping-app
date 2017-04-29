9550adf3902f757778c5b93db8fba8e4fdd8b644
Yogesh H <you@example.com>
2017-04-29 16:45:51 +0530

    database configured

--- app/config/index.js
+++ app/config/index.js
@@ -2,7 +2,7 @@ let credentials = require('./credentials');
 
 module.exports = {
 	dbConnection: () => {
-		// return "mongodb://"+ credentials.username +":"+ credentials.password +"@ds131340.mlab.com:31340/firstappdb";
-		return "mongodb://localhost/shopping-app";
+		return "mongodb://"+ credentials.username +":"+ credentials.password +"@ds125481.mlab.com:25481/shopping-app"
+		// return "mongodb://localhost/shopping-app";
 	}
 }
