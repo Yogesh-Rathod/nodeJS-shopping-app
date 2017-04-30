906bd1a4612745cbcf818780e1962cbfb34b6885
Yogesh H <you@example.com>
2017-04-30 11:21:37 +0530

    Dynamic Database  Config String

--- app/config/index.js
+++ app/config/index.js
@@ -1,8 +1,13 @@
 let credentials = require('./credentials');
 
+let databaseString = "mongodb://localhost/shopping-app";
+
+if (process.env.NODE_ENV) {
+	databaseString = "mongodb://"+ credentials.username +":"+ credentials.password +"@ds125481.mlab.com:25481/shopping-app";
+}
+
 module.exports = {
 	dbConnection: () => {
-		return "mongodb://"+ credentials.username +":"+ credentials.password +"@ds125481.mlab.com:25481/shopping-app";
-		// return "mongodb://localhost/shopping-app";
+		return databaseString;
 	}
 }
